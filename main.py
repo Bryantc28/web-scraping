38e7e5b327a1aef33b82f4fdd74f23117e5b2856
Bryant <bryaantc28@gmail.com>
2023-11-20 11:28:04 -0500

    add functionality to the send_email function

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import requests
 import selectorlib
+import os
+import smtplib
+import ssl
 
 url = "https://programmer100.pythonanywhere.com/tours/"
 headers = {
@@ -19,8 +22,18 @@ def extract(source):
     value = extractor.extract(source)["tours"]
     return value
 
-def send_email():
-    print("Email was sent!")
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "acaldezuniga@gmail.com"
+    password = os.getenv("PASSWORD")
+    receiver = "acaldezuniga@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
 
 
 def store(extracted):
@@ -32,6 +45,7 @@ def read(extracted):
     with open("data.txt", 'r') as file:
         return file.read()
 
+
 if __name__ == "__main__":
     scraped = scrape(url)
     extracted = extract(scraped)
@@ -40,5 +54,5 @@ if __name__ == "__main__":
     if extracted != "No upcoming tours":
         if extracted not in content:
             store(extracted)
-            send_email()
+            send_email(message="Hey, new event was found!")
 
